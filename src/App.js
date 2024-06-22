096c1631b1ff22e176e823918ee73699b910b552
Diêgo Fontes <104260227+DigFontes@users.noreply.github.com>
2024-06-22 17:12:22 -0300

    Aplicado o conceitos de componentização a todos os elementos possíveis de componentizar.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import Header from "./componentes/Header";
 import Persquisa from "./componentes/Pesquisa";
-import styled from "styled-components";
 import UltimosLancamentos from "./componentes/UltimosLacnamentos";
-
-const AppContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(90deg, #002f52 35%, #326589);
-`;
+import { AppContainer } from "./componentes/AppContainer";
 
 function App() {
   return (
